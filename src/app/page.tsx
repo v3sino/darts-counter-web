9de4a77136eb2e6833d03483c38ba7217e478e40
Alžbeta Hajná <ahajna@typefot.io>
2023-11-22 17:16:19 +0100

    Prrof of concept of firebase db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-export default function Home() {
+import { db } from "@/firebase";
+import { collection, getDocs, query } from "firebase/firestore";
+
+export default async function Home() {
+  const invitesCollection = collection(db, "invites");
+  const invitesQuery = query(invitesCollection);
+  const querySnapshot = await getDocs(invitesQuery);
+  querySnapshot.forEach((doc) => {
+    console.log(doc.data());
+  });
+
   return (
     <main>
       <div className="flex h-screen">
         <h1>Main page with info about app</h1>
       </div>
     </main>
-    
-  )
-};
+  );
+}
