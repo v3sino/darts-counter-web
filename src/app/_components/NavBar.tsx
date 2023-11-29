b8b068afae1ea6be58f095e661edd49379d87296
Alžbeta Hajná <betka@hajny.sk>
2023-11-29 21:44:21 +0100

    Highlight current path in Navbar

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,17 +1,41 @@
+'use client';
+
 import Link from 'next/link';
 import LoginStatus from '@/app/_components/LoginStatus';
 import ProfileButton from '@/app/_components/ProfileButton';
+import { usePathname } from 'next/navigation';
 
-export const NavBar = () => (
-	<div className="sticky top-0 flex justify-between bg-blue-400 p-4 text-white">
-		<div className="flex space-x-4">
-			<Link href="/">Home</Link>
-			<Link href="/statistics">Statistics</Link>
-			<Link href="/tournament">Tournament</Link>
+const homePath = '/';
+const statisticsPath = '/statistics';
+const tournamentsPath = '/tournaments';
+
+export const NavBar = () => {
+	const currentPath = usePathname();
+
+	const isLinkActive = (path: string) => {
+		return currentPath === path
+			? 'bg-blue-200 hover:bg-blue-300 text-black p-2 shadow-md rounded-xl'
+			: 'p-1';
+	};
+
+	const getLink = (path: string, name: string) => {
+		return (
+			<Link href={`${path}`} className={isLinkActive(path)}>
+				{name}
+			</Link>
+		);
+	};
+	return (
+		<div className="sticky top-0 flex justify-between bg-blue-400 p-4 text-white">
+			<div className="flex items-center space-x-4">
+				{getLink(homePath, 'Home')}
+				{getLink(statisticsPath, 'Statistics')}
+				{getLink(tournamentsPath, 'Tournaments')}
+			</div>
+			<div className="flex space-x-4">
+				<ProfileButton />
+				<LoginStatus />
+			</div>
 		</div>
-		<div className="flex space-x-4">
-			<ProfileButton />
-			<LoginStatus />
-		</div>
-	</div>
-);
+	);
+};
