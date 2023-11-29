26bd987563ad90bcda70efa59e96e2479d233d43
Alžbeta Hajná <betka@hajny.sk>
2023-11-29 23:39:10 +0100

    Update mock tournaments ids

--- src/data/tournament_mock.ts
+++ src/data/tournament_mock.ts
@@ -11,14 +11,14 @@ export const demo_tournament_list: Tournament[] = [
         startAt: new Date(),
     },
     {
-        id: '1',
+        id: '2',
         name: 'Demo tournament in very past',
         organizedByUID: 'KafQzU4m5IhPPQDEuDjGgrCf7MC3',
         location: 'Brno',
         startAt: new Date(23, 5, 24),
     },
     {
-        id: '1',
+        id: '3',
         name: 'Demo tournament in future',
         organizedByUID: 'KafQzU4m5IhPPQDEuDjGgrCf7MC3',
         location: 'Brno',
