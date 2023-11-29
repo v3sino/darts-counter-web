7925e088164d9791136b9e1fa9f6da16198b48e9
Martin Slovík <xslovik@fi.muni.cz>
2023-11-29 10:23:34 +0100

    Add resolver and sign up option into SignIn page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,14 +2,16 @@
 
 import { signIn } from 'next-auth/react';
 import { useForm } from 'react-hook-form';
-import { User } from '@/types/user';
+import { User, UserSchema } from '@/types/user';
+import Link from 'next/link';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const SignIn = () => {
 	const {
 		register,
 		handleSubmit,
-		formState: { errors }
-	} = useForm<User>();
+		formState: { errors, isSubmitting }
+	} = useForm<User>({ resolver: zodResolver(UserSchema) });
 
 	const onSubmit = async (data: User) => {
 		await signIn('credentials', {
@@ -45,7 +47,7 @@ const SignIn = () => {
 										type="email"
 										autoComplete="email"
 										{...register('email', { required: true })}
-										className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+										className="block w-full rounded-md border-0 bg-white/5 p-2 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
 									/>
 									{errors.email && (
 										<span className="text-red-500">Email is required</span>
@@ -68,7 +70,7 @@ const SignIn = () => {
 										type="password"
 										autoComplete="current-password"
 										{...register('password', { required: true })}
-										className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+										className="block w-full rounded-md border-0 bg-white/5 p-2 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
 									/>
 									{errors.password && (
 										<span className="text-red-500">Password is required</span>
@@ -79,14 +81,18 @@ const SignIn = () => {
 							<div>
 								<button
 									type="submit"
+									disabled={isSubmitting}
 									className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 disabled:opacity-40"
 								>
-									Sign in
+									{isSubmitting ? 'Signing In...' : 'Sign in'}
 								</button>
 							</div>
 						</div>
 					</div>
 				</form>
+				<Link href="/signup" className="mt-4 text-center text-white underline">
+					Do not have an account? Sign up
+				</Link>
 			</div>
 		</>
 	);
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,10 +1,13 @@
 import { z } from 'zod';
 
-const emailSchema = z.string().email('Invalid email format');
-
-const passwordSchema = z
+const emailSchema = z
 	.string()
-	.min(6, 'Password must be at least 6 characters long');
+	.email()
+	.refine(value => /\S+@\S+\.\S+/.test(value), {
+		message: 'Invalid email format'
+	});
+
+const passwordSchema = z.string().min(6, 'Password too short');
 
 export const UserSchema = z.object({
 	email: emailSchema,
