123ed57d475da27af582ccf5ccd6c6f92dfa71ee
Alžbeta Hajná <betka@hajny.sk>
2023-12-02 19:13:27 +0100

    Extract spinner from LoadingPage

---
+++ src/app/_components/LoadingSpinner.tsx
@@ -0,0 +1,5 @@
+export const LoadingSpinner = () => (
+	<div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-gray-900 bg-opacity-75">
+		<div className="h-16 w-16 animate-spin rounded-full border-t-4 border-solid border-blue-500" />
+	</div>
+);
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,7 @@
 // taken from task 05
 
-const LoadingPage = () => (
-	<div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-gray-900 bg-opacity-75">
-		<div className="h-16 w-16 animate-spin rounded-full border-t-4 border-solid border-blue-500" />
-	</div>
-);
+import { LoadingSpinner } from './_components/LoadingSpinner';
+
+const LoadingPage = () => <LoadingSpinner />;
 
 export default LoadingPage;
