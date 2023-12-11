f4e03cb594faac2f97f74220f1ae85fa401e6660
Alžbeta Hajná <betka@hajny.sk>
2023-12-11 20:42:53 +0100

    Listen on games

--- src/app/_components/tournament/TournamentTable.tsx
+++ src/app/_components/tournament/TournamentTable.tsx
@@ -1,14 +1,50 @@
 'use client';
 
+import { collection } from 'firebase/firestore';
 import { SendInviteButton } from './ActionButton';
 import { StatusBadge } from './StatusBadge';
 import { Invite, InviteCreate, InviteStatus } from '@/types/invite';
+import { db } from '@/firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { useEffect, useState } from 'react';
+import { Game } from '@/types/game';
 
 interface TableProps {
 	invites: Invite[];
 }
 
+type GameStatesMap = Record<string, string>;
+
 export const TournamentTable = ({ invites: invites }: TableProps) => {
+	const [games, gamesLoading, gamesError] = useCollection(
+		collection(db, 'games'),
+		{
+			snapshotListenOptions: { includeMetadataChanges: true }
+		}
+	);
+	const [gameStates, setGameStates] = useState<GameStatesMap>({});
+
+	useEffect(() => {
+		const newGameStates: GameStatesMap = {};
+		games?.docs.forEach(gameDoc => {
+			const game = gameDoc.data() as Game;
+			if (game.gameState.legEnded) {
+				if (game.gameState.currentPlayer == 0) {
+					newGameStates[gameDoc.id] = '1:0';
+				} else {
+					newGameStates[gameDoc.id] = '0:1';
+				}
+			} else {
+				newGameStates[gameDoc.id] = 'game in progress';
+			}
+		});
+		setGameStates(newGameStates);
+	}, [games]);
+
+	const getGameScoreFromInviteId = (inviteId: string) => {
+		return gameStates[inviteId] || 'not started yet';
+	};
+
 	const renderActionForInvite = (
 		status: InviteStatus,
 		inviteCreate: InviteCreate
@@ -26,6 +62,7 @@ export const TournamentTable = ({ invites: invites }: TableProps) => {
 				return null;
 		}
 	};
+
 	return (
 		<div className="overflow-x-auto rounded-lg">
 			<table className="w-full text-left text-sm text-gray-400">
@@ -42,6 +79,9 @@ export const TournamentTable = ({ invites: invites }: TableProps) => {
 						</th>
 						<th scope="col" className="px-6 py-3">
 							action
+						</th>
+						<th scope="col" className="px-6 py-3">
+							game score
 						</th>
 					</tr>
 				</thead>
@@ -64,6 +104,9 @@ export const TournamentTable = ({ invites: invites }: TableProps) => {
 										inviteFromUID: invite.inviteFromUID,
 										inviteToUID: invite.inviteToUID
 									})}
+								</td>
+								<td className="px-6 py-4">
+									{getGameScoreFromInviteId(invite.id)}
 								</td>
 							</tr>
 						);
