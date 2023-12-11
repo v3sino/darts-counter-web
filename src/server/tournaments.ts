a642741d31fec75326a3ac1444b70871581cf86a
Alžbeta Hajná <betka@hajny.sk>
2023-12-11 19:15:49 +0100

    Remove unused functions and imports

--- src/server/tournaments.ts
+++ src/server/tournaments.ts
@@ -1,7 +1,7 @@
 import { db } from "@/firebase";
 import { Invite, InviteStatus, inviteConverter } from "@/types/invite";
-import { Tournament, TournamentRecord, TournamentRecordStatus } from "@/types/tournament";
-import { DocumentData, Query, QuerySnapshot, Timestamp, collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
+import { Tournament, TournamentRecord } from "@/types/tournament";
+import { DocumentData, QuerySnapshot, Timestamp, collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
 
 type idProps = {
   id: string
@@ -14,33 +14,6 @@ export async function getTournamentById({ id }: idProps) {
   if (docSnap.exists()) {
     return docSnap.data();
   }
-}
-
-export function getQueryInvitesFrom(inviteFromUID: string): Query<DocumentData, DocumentData> {
-  const invitesRef = collection(db, 'invites');
-  const q = query(invitesRef, where('inviteFromUID', '==', inviteFromUID));
-  return q;
-}
-
-async function getRecordByUID(uid: string, invitesCollection:  QuerySnapshot<DocumentData, DocumentData>) {
-  console.log(`getting tournament by UID ${uid}`);
-
-  const userDocSnap = await getDoc(doc(db, "users", uid));
-  if (userDocSnap.exists()) {
-    var inviteTo = userDocSnap.id;
-    var userDocData = userDocSnap.data();
-    userDocData.status = TournamentRecordStatus.NotInvitedYet;
-
-    if (invitesCollection != null) {
-      invitesCollection.docs.map(inviteDoc => {
-        if (inviteTo === inviteDoc.data().inviteToUID) {
-          userDocData.status = inviteDoc.data().status;
-        }
-      });
-    }
-    return convertToTournamentRecord(userDocData, uid);
-  }
-  throw new Error(`Document with ID ${uid} does not exist`);
 }
 
 // TODO: use converter instead with classes? https://firebase.google.com/docs/firestore/manage-data/add-data#custom_objects
@@ -61,7 +34,6 @@ export async function convertToTournament(docData: DocumentData | undefined, doc
         const toUID = docData.records[j];
 
         if (fromUID.length !== 0 && toUID.length !== 0) {
-          console.log(fromUID, toUID);
           try {
             const invite = await fetchInvite(fromUID, toUID);
             invites.push(invite);
@@ -92,7 +64,6 @@ async function fetchInvite(fromUID: string, toUID: string): Promise<Invite> {
   var invite;
 
   querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
     invite = inviteConverter.fromFirestore(doc);
   });
 
@@ -113,27 +84,7 @@ async function fetchInvite(fromUID: string, toUID: string): Promise<Invite> {
   return invite;
 }
 
-function convertTournamentRecordStatus(statusString: string): TournamentRecordStatus {
-  if (Object.values(TournamentRecordStatus).includes(statusString as TournamentRecordStatus)) {
-    return statusString as TournamentRecordStatus;
-  }
-  return TournamentRecordStatus.NotInvitedYet;
-}
-
-export function convertToTournamentRecord(docData: any, uid: string): TournamentRecord {
-  if (!docData || typeof docData !== 'object') {
-    throw new Error('Invalid document data');
-  }
-
-  return {
-    uid: uid,
-    username: docData.username ?? '',
-    inviteHash: docData.inviteHash ?? '',
-    status: convertTournamentRecordStatus(docData.status),
-    // statusTimestamp: docData.startAt instanceof Timestamp ? docData.startAt.toDate() : new Date(),
-  };
-}
-
+// TODO(betka): remove this by using converter
 export async function remapResponseWithStartAt(response: Response) {
   const fetchedTournaments = await response.json();
 
