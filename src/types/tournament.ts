343b97cb509772677da1204cc0d2ec32a9a9cd0f
Alžbeta Hajná <betka@hajny.sk>
2023-12-12 10:01:11 +0100

    format

--- src/types/tournament.ts
+++ src/types/tournament.ts
@@ -1,49 +1,48 @@
-import { string, z } from "zod"
-import { Invite } from "./invite";
+import { string, z } from 'zod';
+import { Invite } from './invite';
 
 export enum TournamentRecordStatus {
-    Rejected = "rejected",
-    Accepted = "accepted",
-    NotInvitedYet = "not invited yet",
-    Pending = "pending"
+	Rejected = 'rejected',
+	Accepted = 'accepted',
+	NotInvitedYet = 'not invited yet',
+	Pending = 'pending'
 }
 
 export type TournamentRecord = {
-    uid: string,
-    username: string,
-    inviteHash: string,
-    status: TournamentRecordStatus,
-    // statusTimestamp?: Timestamp,
-}
+	uid: string;
+	username: string;
+	inviteHash: string;
+	status: TournamentRecordStatus;
+	// statusTimestamp?: Timestamp,
+};
 
 export type Tournament = {
-    id: string,
-    name: string,
-    organizedByUID: string,
-    location: string,
-    startAt: Date,
-    records: TournamentRecord[],
-    invites: Invite[],
-}
+	id: string;
+	name: string;
+	organizedByUID: string;
+	location: string;
+	startAt: Date;
+	records: TournamentRecord[];
+	invites: Invite[];
+};
 
 export const TournamentSchema = z.object({
-    name: string().min(1),
-    location: string().min(1),
-    startAt: string(),
+	name: string().min(1),
+	location: string().min(1),
+	startAt: string()
 });
 
 export const TournamentCreateSchema = TournamentSchema.extend({
-    organizedByUID: string().min(1),
+	organizedByUID: string().min(1)
 });
 
 export type TournamentCreate = z.infer<typeof TournamentCreateSchema>;
 
-
 export const TournamentUpdateSchema = z.object({
-    // name: z.string().min(1).optional(),
-    // location: z.string().min(1).optional(),
-    // startAt: z.string().min(1).optional(),
-    records: z.array(z.string()).optional(),
+	// name: z.string().min(1).optional(),
+	// location: z.string().min(1).optional(),
+	// startAt: z.string().min(1).optional(),
+	records: z.array(z.string()).optional()
 });
 
 export type TournamentUpdate = z.infer<typeof TournamentUpdateSchema>;
