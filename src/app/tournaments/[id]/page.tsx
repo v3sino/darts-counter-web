012e8f9db846b52548015fcf68299c90773b388b
Alžbeta Hajná <betka@hajny.sk>
2023-12-12 10:40:04 +0100

    flag icon to startAt

--- src/app/tournaments/[id]/page.tsx
+++ src/app/tournaments/[id]/page.tsx
@@ -5,7 +5,10 @@ import { DeleteTournamentButton } from '@/app/_components/tournament/DeleteTourn
 import { TournamentTable } from '@/app/_components/tournament/TournamentTable';
 import { UserSelection } from '@/app/_components/tournament/UserSelection';
 import { useTournamentData } from '@/app/_hooks/tournament';
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import {
+	faFlagCheckered,
+	faLocationDot
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import toast from 'react-hot-toast';
@@ -35,14 +38,16 @@ const TournamentPage = ({ params }: TournamentPageProps) => {
 				<div className="p-4 text-white sm:p-12">
 					<h1 className="mb-8 text-4xl font-bold">{tournamentData.name}</h1>
 					<div className="flex flex-col sm:flex-row sm:items-center">
-						<div className="sm:pr-4">
+						<div className="pb-1 sm:pr-4">
 							<FontAwesomeIcon icon={faLocationDot} className="mr-2" />
 							{tournamentData?.location}
 						</div>
-						<div className="sm:pr-4">
-							Start: {tournamentData?.startAt.toDateString()}
+						<div className="pb-1 sm:pr-4">
+							<FontAwesomeIcon icon={faFlagCheckered} className="mr-2" />
+							{tournamentData?.startAt.toLocaleDateString()}{' '}
+							{tournamentData?.startAt.toLocaleTimeString()}
 						</div>
-						<div className="sm:pr-4">
+						<div className="pb-1 sm:pr-4">
 							<DeleteTournamentButton id={params.id} />
 						</div>
 					</div>
