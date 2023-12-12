99e616bba639a6b600b0b7f8d76cb24cbcbdf4ab
Alžbeta Hajná <betka@hajny.sk>
2023-12-12 10:21:43 +0100

    format

--- src/app/api/tournaments/route.ts
+++ src/app/api/tournaments/route.ts
@@ -1,48 +1,59 @@
-import { db } from "@/firebase";
-import { TournamentCreateSchema } from "@/types/tournament";
-import { Timestamp, addDoc, collection, getDocs, query, where } from "firebase/firestore";
-import { NextRequest } from "next/server";
-import { z } from "zod";
+import { db } from '@/firebase';
+import { TournamentCreateSchema } from '@/types/tournament';
+import {
+	Timestamp,
+	addDoc,
+	collection,
+	getDocs,
+	query,
+	where
+} from 'firebase/firestore';
+import { NextRequest } from 'next/server';
+import { z } from 'zod';
 
 // creates a new tournament
 export const POST = async (request: NextRequest) => {
-  // TODO: move to server?
-  try {
+	// TODO: move to server?
+	try {
+		const data = await request.json();
 
-	const data = await request.json();
+		const parsedTournament = TournamentCreateSchema.parse(data);
 
-	const parsedTournament = TournamentCreateSchema.parse(data);
+		data.startAt = Timestamp.fromDate(new Date(data.startAt));
 
-    data.startAt = Timestamp.fromDate(new Date(data.startAt));
-
-    await addDoc(collection(db, "tournaments"), data);
+		await addDoc(collection(db, 'tournaments'), data);
 
 		return Response.json(parsedTournament, {
 			status: 201
 		});
 	} catch (error) {
 		if (error instanceof z.ZodError) {
-      console.log(error);
+			console.log(error);
 			return Response.json({ errors: error.errors }, { status: 400 });
 		} else {
 			return Response.json({ message: 'Unexpected error' }, { status: 500 });
 		}
 	}
-}
+};
 
 export const GET = async (request: NextRequest) => {
 	// TODO: move to server?
-	var uid = request.nextUrl.searchParams.get("uid");
+	var uid = request.nextUrl.searchParams.get('uid');
 
-	var q = query(collection(db, "tournaments"));
+	var q = query(collection(db, 'tournaments'));
 	if (uid != null) {
 		console.log(`uid: ${uid}}`);
-		q = query(collection(db, "tournaments"), where("organizedByUID", "==", uid));
+		q = query(
+			collection(db, 'tournaments'),
+			where('organizedByUID', '==', uid)
+		);
 	}
 
 	const querySnapshot = await getDocs(q);
-	const tournaments = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-	console.log(tournaments);
+	const tournaments = querySnapshot.docs.map(doc => ({
+		id: doc.id,
+		...doc.data()
+	}));
 
 	return Response.json(tournaments);
-  }
+};
